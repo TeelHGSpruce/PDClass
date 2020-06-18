38851e80117eb3759962ff0e2d5213df70ab120e
TeelHGSpruce <67081842+TeelHGSpruce@users.noreply.github.com>
2020-06-18 14:37:29 -0500

    Add files via upload
    Adding the counter

---
+++ mycounter.js
@@ -0,0 +1,16 @@
+var currentCount = parseInt(documentById("countNumberDisplay").innerHTML, 10);
+
+documet.getElementID("up").addEvenetListerner(click, function(){
+document.getElementById("countNumDisplay").inmnerHTML = currentCount++;
+}
+
+documet.getElementID("down").addEvenetListerner(click, function(){
+    document.getElementById("countNumDisplay").inmnerHTML = currentCount--;
+    
+    }
+    
+
+
+
+
+}}
